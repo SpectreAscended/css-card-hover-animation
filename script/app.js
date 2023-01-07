7c9ceb249b336a7db943eebca90bc6654f5d2406
Cory Pollard <astandard87@gmail.com>
2023-01-07 14:17:03 -0600

    Added audio

---
+++ script/app.js
@@ -0,0 +1,22 @@
+const meinPorsche = new Audio('../assets/mein-porsche.mp3');
+
+const audioBtn = document.querySelector('.play-audio');
+
+const audioHandler = function () {
+  let clicked = false;
+
+  audioBtn.addEventListener('click', function () {
+    if (!clicked) {
+      meinPorsche.play();
+      audioBtn.innerText = 'Pause Audio';
+    } else {
+      meinPorsche.pause();
+
+      audioBtn.innerText = 'Play Audio';
+    }
+    clicked = !clicked;
+    console.log(clicked);
+  });
+};
+
+audioHandler();
